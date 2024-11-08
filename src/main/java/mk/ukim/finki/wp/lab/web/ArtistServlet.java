3d36957dbbb21866cfdfe9b09b145117ce1e02a9
MilanBornarov <milan.bornarov1@gmail.com>
2024-11-08 13:52:30 +0100

    Final

--- src/main/java/mk/ukim/finki/wp/lab/web/ArtistServlet.java
+++ src/main/java/mk/ukim/finki/wp/lab/web/ArtistServlet.java
@@ -5,7 +5,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import mk.ukim.finki.wp.lab.service.SongService;
+import mk.ukim.finki.wp.lab.service.ArtistService;
 import org.thymeleaf.context.WebContext;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.web.IWebExchange;
@@ -13,30 +13,34 @@ import org.thymeleaf.web.servlet.JakartaServletWebApplication;
 
 import java.io.IOException;
 
-@WebServlet(name="SongListServlet", urlPatterns = "/listSongs")
-public class SongListServlet extends HttpServlet {
+@WebServlet(name = "ArtistServlet", urlPatterns = "/artist")
+public class ArtistServlet extends HttpServlet {
 
-    private final SongService songService;
+    private final ArtistService artistService;
     private final SpringTemplateEngine templateEngine;
 
-    public SongListServlet(SongService songService, SpringTemplateEngine templateEngine) {
-        this.songService = songService;
+    public ArtistServlet(ArtistService artistService, SpringTemplateEngine templateEngine) {
+        this.artistService = artistService;
         this.templateEngine = templateEngine;
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String trackId = (String) req.getSession().getAttribute("trackId");
         IWebExchange webExchange = JakartaServletWebApplication.buildApplication(getServletContext()).buildExchange(req, resp);
         WebContext webContext = new WebContext(webExchange);
 
-        webContext.setVariable("songs", songService.listSongs());
-        templateEngine.process("listSongs", webContext, resp.getWriter());
+        webContext.setVariable("artists", artistService.listArtists());
+        webContext.setVariable("trackId", trackId);
+        templateEngine.process("artistsList", webContext, resp.getWriter());
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String trackId = req.getParameter("trackId");
-        req.getSession().setAttribute("trackId", trackId);
-        resp.sendRedirect("/artist");
+        String artistId = req.getParameter("artistId");
+        String trackId = (String) req.getSession().getAttribute("trackId");
+
+        req.getSession().setAttribute("artistId", artistId);
+        resp.sendRedirect("/songDetails");
     }
 }
--- src/main/java/mk/ukim/finki/wp/lab/web/SongDetailsServlet.java
+++ src/main/java/mk/ukim/finki/wp/lab/web/SongDetailsServlet.java
@@ -5,6 +5,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import mk.ukim.finki.wp.lab.model.Song;
 import mk.ukim.finki.wp.lab.service.SongService;
 import org.thymeleaf.context.WebContext;
 import org.thymeleaf.spring6.SpringTemplateEngine;
@@ -13,30 +14,26 @@ import org.thymeleaf.web.servlet.JakartaServletWebApplication;
 
 import java.io.IOException;
 
-@WebServlet(name="SongListServlet", urlPatterns = "/listSongs")
-public class SongListServlet extends HttpServlet {
+@WebServlet(name = "SongDetailsServlet", urlPatterns = "/songDetails")
+public class SongDetailsServlet extends HttpServlet {
 
     private final SongService songService;
     private final SpringTemplateEngine templateEngine;
 
-    public SongListServlet(SongService songService, SpringTemplateEngine templateEngine) {
+    public SongDetailsServlet(SongService songService, SpringTemplateEngine templateEngine) {
         this.songService = songService;
         this.templateEngine = templateEngine;
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String trackId = (String) req.getSession().getAttribute("trackId");
+        Song song = songService.findByTrackId(trackId);
+
         IWebExchange webExchange = JakartaServletWebApplication.buildApplication(getServletContext()).buildExchange(req, resp);
         WebContext webContext = new WebContext(webExchange);
 
-        webContext.setVariable("songs", songService.listSongs());
-        templateEngine.process("listSongs", webContext, resp.getWriter());
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String trackId = req.getParameter("trackId");
-        req.getSession().setAttribute("trackId", trackId);
-        resp.sendRedirect("/artist");
+        webContext.setVariable("song", song);
+        templateEngine.process("songDetails", webContext, resp.getWriter());
     }
 }
